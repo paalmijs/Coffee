713c550855a5beed7b869bcd87ab1f17320e4861
paalmijs <palmijskruts@gmail.com>
2022-08-28 18:24:47 +0300

    Fixed a few problems.

--- coffee.py
+++ coffee.py
@@ -1,7 +1,11 @@
 import discord
 from discord.ext import commands
 
+bot = commands.Bot(command_prefix="esp")
+
 #Prefix = esp
+#Invite link: https://discord.com/oauth2/authorize?client_id=1013465974881660999&scope=bot&permissions=8
+
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name="^help priekš vairāk informācijas!"))
@@ -18,6 +22,9 @@ async def on_message(message):
     if message.content == 'esptest':
         await message.channel.send(f' response')
 
+    if message.content == 'espstory':
+        await message.channel.send(f'There really is no story I just like coffee!')
+
     await bot.process_commands(message)
 
-bot.run('')
+bot.run('MTAxMzQ2NTk3NDg4MTY2MDk5OQ.GGxKY4.pu2ZT922pvyxdtUBKM4vvhyZ-F8Jkxby2SIbHU')
