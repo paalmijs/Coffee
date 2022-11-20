b10d280f8e05a4dc6ed8e0c9b3ae1b8f297e681b
paalmijs <palmijskruts@gmail.com>
2022-11-21 00:26:48 +0200

    Update coffee.py

--- coffee.py
+++ coffee.py
@@ -4,23 +4,14 @@ import os
 
 from discord.ext import commands
 from discord.utils import get
-from discord import FFmpegPCMAudio
 from discord import TextChannel
-from youtube_dl import YoutubeDL
 
 
-client = commands.Bot(command_prefix='*')  # prefix our commands with 'esp!'
 
-#Invite link: https://discord.com/oauth2/authorize?client_id=1013465974881660999&scope=bot&permissions=8
-
+client = commands.Bot(command_prefix='*')  # prefix our commands with "*"
 
 @client.command()
 async def ping(ctx):
     await ctx.send("Pong!")
 
-@client.command()
-async def clear(ctx, amount=10):
-    await ctx.channel.purge(limit=amount)
-    await ctx.send("Messages have been deleted!")
-
-client.run('')
+client.run('MTA0MjgwNDg2MDAxMTE3MTg5MQ.G7z8zV.eDwCGArE4rjWTgRsbdgrEtbl80bAxTR9yCDKEE')
